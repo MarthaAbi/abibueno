245eebfe32c39bf568ad8817203ac9f5d340ec55
Marthacruzsanchez <martha151998@gmail.com>
2024-10-21 16:55:35 +0200

    subida 8

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-const instance = axios.create({ baseURL: 'http://localhost:8080/people' });
+const url = 'https://abi-2.onrender.com/' 
+// const url ='http://localhost:8080/people' 
+const instance = axios.create({ baseURL: url});
+
 
 export const savePersonName = async (obj) => await instance.post('/', obj)
 
